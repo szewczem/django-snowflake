8cfc8f6e9bca1be24ff1af03f871692df6f84829
szewczem <szewczem@gmail.com>
2025-05-04 13:15:44 +0200

    updated equipment model

--- reservations/models.py
+++ reservations/models.py
@@ -21,7 +21,7 @@ class Equipment(models.Model):
     name = models.CharField(max_length=64)
     length = models.CharField(max_length=3, null=True)
     level = models.CharField(max_length=20, null=True)
-    banner = models.ImageField(default="equipment_photo/test.jpg", blank=True)
+    banner = models.ImageField(default="{{ EQUIPMENT_PHOTO_TEST }}", blank=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
