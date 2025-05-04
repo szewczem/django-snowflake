acdbfa82e3259e6893558e0c38282dd74dabe8ac
szewczem <szewczem@gmail.com>
2025-05-04 12:54:39 +0200

    updated equipment model

--- reservations/models.py
+++ reservations/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 class Equipment(models.Model):   
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='category')
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
     length = models.CharField(max_length=3, null=True)
     level = models.CharField(max_length=20, null=True)
     banner = models.ImageField(default="equipment_photo/test.jpg", blank=True)
